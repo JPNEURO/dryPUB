1f743d68d6c34878ccda4cb78cd7dbb853289c3d
Joshua_Podmore <joshpodmore@hotmail.co.uk>
2020-02-28 20:38:51 +0000

    Repo Info Update

---
+++ repo_info.py
@@ -0,0 +1,20 @@
+# REPO INFO: github / atom initialization.
+
+# Followed instructions via: https://www.youtube.com/watch?v=6HsZMl-qV5k
+
+# Set-up github for atom by generating git token: https://github.atom.io/auth/github_package/token
+
+# Ensure gitconfig file present on PC (should be if Atom installed).
+     # Add this to all gitconfig files on machine (should be in atom sub-directory)
+
+     # [user]
+     #    name = Joshua_Podmore
+     #    email = joshpodmore@hotmail.co.uk
+
+# Method for updating git-repo:
+
+     # 1) Save file currently working on.
+     # 2) Open Git tab in top right of screen, should see files in unstaged changes.
+     # 3) Type brief commentin 'Commit Message' box.
+     # 4) Select 'Commit to <REPO NAME>'
+     # 5)
